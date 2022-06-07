883e8e34f565952b44eb081b8b97a13a467fd934
ericsanchezgonzalez <91422878+ericsanchezgonzalez@users.noreply.github.com>
2022-06-07 15:27:25 +0200

    Commit modificacions initSettings.m per llegir senyals TEXBAT

--- GNSS_SDR/initSettings.m
+++ GNSS_SDR/initSettings.m
@@ -55,14 +55,27 @@ settings.skipNumberOfBytes     = 0;
 % This is a "default" name of the data file (signal record) to be used in
 % the post-processing mode
 settings.fileName           = ...
-   'C:\Users\erics\OneDrive\Documentos\MATLAB\TFG\Borre\GNSS_signal_records\GPSdata-DiscreteComponents-fs38_192-if9_55.bin';
+    'D:\TexBat_spoofedsignals\ds3.bin';%ds3
+    %'C:\Users\erics\OneDrive\Documentos\MATLAB\TFG\Borre\GNSS_signal_records\GPSdata-DiscreteComponents-fs38_192-if9_55.bin';
+    %'D:\texbat_signals\cleanStatic.bin';%Clean static
+    
 % Data type used to store one sample
-settings.dataType           = 'int8';
+settings.dataType           = 'int16'; %texbat
+% settings.dataType           = 'int8';
 
 % Intermediate, sampling and code frequencies
-settings.IF                 = 9.548e6;      %[Hz]
-settings.samplingFreq       = 38.192e6;     %[Hz]
+%*******************TEXBAT SIGNALS*****************************************
+settings.IF                 = 0;%1575420000
+settings.samplingFreq       = 25e6;     %[Hz]
+% % Define number of chips in a code period
 settings.codeFreqBasis      = 1.023e6;      %[Hz]
+%************************************************************************
+
+%******************GPSdata-DiscreteComponents-fs38_192-if9_55.bin********
+% settings.IF                 = 9.548e6;%[Hz]
+% settings.samplingFreq       = 38.192e6;     %[Hz]
+% settings.codeFreqBasis      = 1.023e6;      %[Hz]
+%************************************************************************
 
 % Define number of chips in a code period
 settings.codeLength         = 1023;
@@ -74,10 +87,12 @@ settings.skipAcquisition    = 1;
 % up acquisition
 settings.acqSatelliteList   = 1:32;         %[PRN numbers]
 % Band around IF to search for satellite signal. Depends on max Doppler
-settings.acqSearchBand      = 14;           %[kHz]
+settings.acqSearchBand      = 14;
+% settings.acqSearchBand      = 14;           %[kHz]
 % Threshold for the signal presence decision rule
-settings.acqThreshold       = 2.5;
-
+settings.acqThreshold       = 1.4;%ds3
+% settings.acqThreshold       = 1.7; %clean static
+% settings.acqThreshold       = 2.5;
 %% Tracking loops settings ================================================
 % Code tracking loop parameters
 settings.dllDampingRatio         = 0.7;
